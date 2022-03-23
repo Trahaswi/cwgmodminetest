4afe4883e0fae4dd1b04a69313120982ff38e125
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2022-03-23 11:23:46 +0530

    commented
    later if possible we move on to nodes.lua rather then messing up with init.lua

--- nodes.lua
+++ nodes.lua
@@ -1,5 +1,5 @@
---minetest.register_tool("cwg:blackstone", {
-  --  description = "blackstone",
-    --inventory_image = "cwg_blackstone_up.png",
-    --sound = {breaks = "default_tool_breaks"},
-}) --
+--[[minetest.register_tool("cwg:blackstone", {
+   description = "blackstone",
+   inventory_image = "cwg_blackstone_up.png",
+   sound = {breaks = "default_tool_breaks"},
+}) ]]--
